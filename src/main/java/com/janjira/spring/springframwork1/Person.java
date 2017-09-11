70705baa1ca96b68216ee95670d194796ff0979a
janjiraGIT <janjira.eriksson@gmial.com>
2017-09-11 20:47:25 +0200

    Added property texId

--- src/main/java/com/janjira/spring/springframwork1/Person.java
+++ src/main/java/com/janjira/spring/springframwork1/Person.java
@@ -3,15 +3,20 @@ package com.janjira.spring.springframwork1;
 public class Person {
 	private int id;
 	private String name;
-	
+	private int taxId;
+
 	public Person() {
-		
+
 	}
-	
+
 	public Person(int id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
+	}
+
+	public void setTaxId(int taxId) {
+		this.taxId = taxId;
 	}
 
 	public void speak() {
@@ -20,6 +25,8 @@ public class Person {
 
 	@Override
 	public String toString() {
-		return "Person [id=" + id + ", name=" + name + "]";
-	}	
+		return "Person [id=" + id + ", name=" + name + ", taxId=" + taxId + "]";
+	}
+
+
 }
